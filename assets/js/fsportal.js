9b6789306696e82101a6e55abf8394660f039788
Seven Du <dujinfang@gmail.com>
2013-03-20 10:17:47 +0800

    show applications and endpoints

--- assets/js/fsportal.js
+++ assets/js/fsportal.js
@@ -12,6 +12,7 @@ App.CallsRoute = Ember.Route.extend({
 		// controller.set('title', "My App");
 		// alert("a")
 		console.log("callsRoute");
+		App.callsController.load();
   	}//,
   	// renderTemplate: function() {
 	// this.render('calls');
@@ -24,15 +25,46 @@ App.ChannelsRoute = Ember.Route.extend({
 		// controller.set('title', "My App");
 		// alert("a")
 		console.log("callsRoute");
+		App.channelsController.load();
   	}//,
   	// renderTemplate: function() {
 		// this.render('calls');
   	// }
 });
 
-App.Router.map(function() {
+
+App.ShowApplicationsRoute = Ember.Route.extend({
+	setupController: function(controller) {
+		// Set the Controller's `title`
+		controller.set('title', "ShowApplications");
+		console.log("showApplications");
+		App.applicationsController.load();
+  	}//,
+  	// renderTemplate: function() {
+		// this.render('calls');
+  	// }
+});
+
+App.ShowEndpointsRoute = Ember.Route.extend({
+	setupController: function(controller) {
+		// Set the Controller's `title`
+		// controller.set('title', "ShowApplications");
+		console.log("showSomething");
+		console.log(controller);
+		App.showEndpointsController.load();
+  	}//,
+  	// renderTemplate: function() {
+		// this.render('calls');
+  	// }
+});
+
+App.Router.map(function(){
 	this.route("calls");
 	this.route("channels");
+	this.route("showApplications");
+	this.route("showEndpoints");
+	this.route("showDialplans");
+	this.route("show");
 	this.route("about", { path: "/about" });
 });
 
@@ -105,10 +137,43 @@ App.channelsController = Ember.ArrayController.create({
 
 });
 
+App.applicationsController = Ember.ArrayController.create({
+	content: [],
+	init: function(){
+	},
+	load: function() {
+		var me = this;
+		$.getJSON("/api/show?application%20as%20json", function(data){
+			  // var channels = JSON.parse(data);
+			console.log(data.row_count);
+			me.set('total', data.row_count);
+			me.content.clear();
+			if (data.row_count == 0) return;
 
+			me.pushObjects(data.rows);
+
+		});
+	}
+});
+
+App.showEndpointsController = Ember.ArrayController.create({
+	content: [],
+	init: function(){
+	},
+	load: function() {
+		var me = this;
+		$.getJSON("/api/show?endpoints%20as%20json", function(data){
+			  // var channels = JSON.parse(data);
+			console.log(data.row_count);
+			me.set('total', data.row_count);
+			me.content.clear();
+			if (data.row_count == 0) return;
+
+			me.pushObjects(data.rows);
+
+		});
+	}
+});
 
 App.initialize();
-
-// App.callsController.load();
-
 
